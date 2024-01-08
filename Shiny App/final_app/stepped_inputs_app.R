23731f90d960fbeee749e4df02e2ddca256c9bf5
mayezharis <mayezharis@gmail.com>
2024-01-08 18:32:47 +0000

    added an About page to app

--- Shiny App/final_app/stepped_inputs_app.R
+++ Shiny App/final_app/stepped_inputs_app.R
@@ -10,13 +10,16 @@
 
 # Different datasets
 
+# TOY DATA
 ToyData1 <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year 5/Dissertation/Code/MMath_Y5_Dissertation/Shiny App/Datasets/final_cleaned_datasets/toy_data_1.csv")
 ToyData2 <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year 5/Dissertation/Code/MMath_Y5_Dissertation/Shiny App/Datasets/final_cleaned_datasets/toy_data_2.csv")
 ToyData3 <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year 5/Dissertation/Code/MMath_Y5_Dissertation/Shiny App/Datasets/final_cleaned_datasets/toy_data_3.csv")
 ToyData4 <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year 5/Dissertation/Code/MMath_Y5_Dissertation/Shiny App/Datasets/final_cleaned_datasets/toy_data_4.csv")
 
+
 # ENERGY DATA
 EnergyData <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year 5/Dissertation/Code/MMath_Y5_Dissertation/Shiny App/Datasets/final_cleaned_datasets/energy_data_cleaned.csv")
+
 
 # NEWS DATA
 NewsData <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year 5/Dissertation/Code/MMath_Y5_Dissertation/Shiny App/Datasets/final_cleaned_datasets/news_data_cleaned.csv")
@@ -30,16 +33,17 @@ BaseballData <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year
 CricketBatData <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year 5/Dissertation/Code/MMath_Y5_Dissertation/Shiny App/Datasets/final_cleaned_datasets/cric_bat_data.csv")
 CricketBowlData <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year 5/Dissertation/Code/MMath_Y5_Dissertation/Shiny App/Datasets/final_cleaned_datasets/cric_bowl_data.csv")
 
+
 # NBA PLAYER DATA
 BasketballData <- read.csv("C:/Users/mayez/OneDrive - University of Edinburgh/Year 5/Dissertation/Code/MMath_Y5_Dissertation/Shiny App/Datasets/final_cleaned_datasets/nba_data_cleaned.csv")
 
 
 
 
-dataset_choices <- c("Toy Dataset 1 (quadratic with noise)" = "toy_data_1",
-                     "Toy Dataset 2 (cubic with noise)" = "toy_data_2",
-                     "Toy Dataset 3 (logarithmic with noise)" = "toy_data_3",
-                     "Toy Dataset 4 (exponential with noise)" = "toy_data_4",
+dataset_choices <- c("Toy Dataset 1 (quadratic)" = "toy_data_1",
+                     "Toy Dataset 2 (cubic)" = "toy_data_2",
+                     "Toy Dataset 3 (logarithmic)" = "toy_data_3",
+                     "Toy Dataset 4 (composite function)" = "toy_data_4",
                      "NBA Player Data" = "nba_data_cleaned",
                      "Cricket Batting Data" = "cric_bat_data",
                      "Cricket Bowling Data" = "cric_bowl_data",
@@ -64,6 +68,22 @@ jscode <- "shinyjs.refresh_page = function() { history.go(0); }" # function to r
 
 ui <-
   navbarPage(
+    tags$head(
+      tags$link(rel="stylesheet", 
+                href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css", 
+                integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ",
+                crossorigin="anonymous"),
+      HTML('<script defer src="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.js" integrity="sha384-2BKqo+exmr9su6dir+qCw08N2ZKRucY4PrGQPPWU1A7FtlCGjmEGFqXCv5nyM5Ij" crossorigin="anonymous"></script>'),
+      HTML('<script defer src="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/contrib/auto-render.min.js" integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI" crossorigin="anonymous"></script>'),
+      HTML('
+    <script>
+      document.addEventListener("DOMContentLoaded", function(){
+        renderMathInElement(document.body, {
+          delimiters: [{left: "$", right: "$", display: false}]
+        });
+      })
+    </script>')
+    ),
     title = "Model Diagnostics",
     tabPanel(title = "Select Data",
              useShinyjs(),
@@ -176,7 +196,109 @@ ui <-
                              plotOutput("fittedLineHqq"),
                              plotOutput("qq_history_plot"))))
              )
-    )
+    ),
+    tabPanel(title = "About",
+             h2("App overview"),
+             p("This app will allow you to construct linear regression models with small samples of various 
+               large datasets. Using this, you may explore and investigate how models might vary under resampling. 
+               The app specifically focuses on how the linear fit of the data and the model diagnostic plots may 
+               vary with each different sample of the data that you take. Along with the linear fit and diagnostic 
+               plots for the current sample, you will also be able to see plots from all previous samples for easy 
+               comparison."),
+             p("It will allow you to explore using toy datasets with predetermined relationships between the 
+               independent and response variables, in addition to real world datasets with more random elements 
+               and no previously known relationships between the various independent and response variables 
+               contained within each of them."),
+             
+             
+             h2("Linear fit"),
+             p("This is a scatter plot of the current sample of the dataset. It allows us to see how the 
+               data points in the current sample are structured. Additionally, we add a linear fit line 
+               to the plot, which represents our linear regression model in the form of a straight line, 
+               given by $\\mathbb{E}(Y_i) = \\alpha + \\beta x_i$ for $i = 1,\\dots,n$."),
+             
+             
+             h2("Model diagnostic plots"),
+             p("When constructing a normal linear regression model, given by 
+               $Y_i \\sim N(\\alpha+\\beta x_i, \\sigma^2)$ for $i = 1, \\dots, n$, we 
+               make the assumptions of:",
+               tags$ol(
+                 tags$li("Linearity of the expectation, $\\mathbb{E}(Y_i) = \\alpha + \\beta x_i$;"),
+                 tags$li("Homoscedasticity (i.e. have the same variance); and"),
+                 tags$li("Normally distributed.")
+                 )),
+             
+             h4("Residuals vs. Fitted"),
+             p("The  first assumption can be verified through the Residuals vs. Fitted plot. This is a scatter 
+               plot of the residuals against fitted values. If the linear assumption is true, then the residuals 
+               should be randomly distributed around the $x$-axis with no discernable pattern or clustering with 
+               respect to the fitted values."),
+             
+             h4("Scale-Location"),
+             p("The second assumption can be verified through the Scale-Location plot. This is a scatter plot 
+               of the absolute values of standardized residuals against fitted values. Here, we are checking 
+               for constant variance, so if the assumption is true, we expect there to be a random scatter around 
+               a horizontal line with no discernable pattern or cluster with respect to the fitted values."),
+             
+             h4("Normal Q-Q"),
+             p("The third assumption can be verified through the Normal Q-Q plot. This is a scatter plot of the 
+               quantiles calculated from the sample data against the quantiles of a standard normal distribution 
+               with mean 0 and standard deviation 1. If the assumption is true, we should see the data points 
+               lying on a straight diagonal line, typically given by the $y=x$ line."),
+             
+             h4("Smoothing line and confidence intervals"),
+             p("Both the Residuals vs. Fitted and Scale-Location plots may also contain a smoothing line to show 
+               the general trend among the data points. While this may not be representative of the data’s 
+               underlying structure, it can be helpful for identifying patterns or clusters which may not be 
+               obvious to the naked eye. It is important to consider the error that may be associated with 
+               calculating this line at each point, and this can be done through the use of a confidence band."),
+             
+             
+             h2("Datasets"),
+             p("This app allows you to choose from a selection of datasets to construct a model from. The toy 
+               datasets have been constructed with predetermined relationships between their respective 
+               independent and response variables."),
+             
+             h3("Toy datasets"),
+             p("These are all datasets with a predetermined relationship between the independent and response 
+               variables. To allow for a more realistic relationship, an element of randomness was included by 
+               adding random noise to each of the response variables. The datasets were all defined by the 
+               independent and response variables below."),
+             h4("Dataset 1 (quadratic):"),
+             p(tags$ul(
+               tags$li("$x_i \\sim N(0,1)$"),
+               tags$li("$y_i = (0.5x_i-1)(0.5x_i+1) + r_i,\\,\\,$ where $\\,r_i \\sim N(0,0.8)$.")
+             )),
+             h4("Dataset 2 (cubic):"),
+             p(tags$ul(
+               tags$li("$x_i \\sim N(0,1)$"),
+               tags$li("$y_i = (x_i-0.5)(-18x_i+0.4)(-x_i-0.5) + r_i,\\,\\,$ where $\\,r_i \\sim N(0,20)$.")
+             )),
+             h4("Dataset 3 (logarithmic):"),
+             p(tags$ul(
+               tags$li("$x_i \\sim N(0,1)$"),
+               tags$li("$y_i = \\mathrm{ln}(x_i + s_i) + r_i,\\,\\,$ where $\\,s_i \\sim N(0,1)\\,\\,$ and 
+                       $\\,r_i \\sim N(0,0.75)$")
+             )),
+             h4("Dataset 4 (composite function):"),
+             p(tags$ul(
+               tags$li("$x_i \\sim \\mathrm{Unif}(-0.25,1)$"),
+               tags$li("$y_i = 0.5\\mathrm{exp}(-20x_i^3) - 0.5 + r_i,\\,\\,$ where $\\,r_i \\sim N(0,0.05)$")
+             )),
+             
+             h3("Real world datasets"),
+             p("When constructing a linear regression model, we are typically aiming to determine whether there 
+               exists a relationship between independent variables and a response variable. Naturally, we aim to 
+               use combinations of independent variables and response variables which could realistically have a 
+               relationship between them. For example, when considering basketball player statistics, it is 
+               understood that while a player playing more minutes per game does not necessarily guarantee they 
+               will have a higher point score, it does increase the likelihood of them having a higher score at 
+               the end of the game."),
+             p("The datasets available on this app are all open source datasets found on the internet. They have 
+               all been modified for the purposes of this app. This includes removing extreme outliers and unwanted 
+               variables. Additionally, random noise of varying degrees has been added to all datasets to make the 
+               data continuous and easier to visualise.")
+             ),
   )
 
 
@@ -653,8 +775,8 @@ server <- function(input, output, session) {
         ggplot(aes_string(x = input$x_var, y = input$y_var)) +
         geom_point(shape = 1) +
         labs(title = "Data Fit") +
-        # stat_smooth(method = "lm", se = FALSE, col="red", linewidth = 0.5) +
-        geom_abline(intercept = coef(mod())[1], slope = coef(mod())[2], color = "red")
+        stat_smooth(method = "lm", se = FALSE, col="red", linewidth = 0.5)
+        # geom_abline(intercept = coef(mod())[1], slope = coef(mod())[2], color = "red")
     })
 
   ######################################################################
